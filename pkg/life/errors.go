7df7d8fde14bb1680ec5aab424faf507d960f9dd
Arhip Efremenko <efremenkoarhip614090@gmail.com>
2025-01-12 14:01:23 +0300

    упростил ошибки

--- pkg/life/errors.go
+++ pkg/life/errors.go
@@ -6,7 +6,5 @@ var (
 	ErrPathFile    error = errors.New("error file path")
 	ErrInvalidFile error = errors.New("error file data")
 
-	ErrWidthWorld      error = errors.New("error range: 100 <= width <= 600")
-	ErrHeightWorld     error = errors.New("error range: 100 <= width <= 300")
-	ErrChanceAliveCell error = errors.New("error range: 1 <= chance <= 100")
+	ErrIncorrectArgs error = errors.New("error incorrect args")
 )
